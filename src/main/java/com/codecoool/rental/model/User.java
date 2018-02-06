b535043c3ab2ad9264f4e589dbf0acd81b7bb921
Abel Nagy <nagy9219abel@gmail.com>
2018-02-06 13:56:48 +0100

    User class basic fields, getters and setters

--- src/main/java/com/codecoool/rental/model/User.java
+++ src/main/java/com/codecoool/rental/model/User.java
@@ -1,5 +1,87 @@
 package com.codecoool.rental.model;
 
+import org.hibernate.annotations.ColumnTransformer;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @ColumnTransformer(read = "pgp_sym_decrypt(password, ‘karalábé‘)", write = "pgp_sym_encrypt(?, ‘karalábé‘)")
+    private String password;
+    private String contacts;
+
+
+    @OneToMany(mappedBy = "user")
+    private List<Reservation> reservations;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password, String contacts) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.contacts = contacts;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(String contacts) {
+        this.contacts = contacts;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", " +
+                "name=" + name;
+    }
 }
