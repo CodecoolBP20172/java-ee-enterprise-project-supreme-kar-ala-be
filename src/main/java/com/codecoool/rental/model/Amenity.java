920ce172bed31f9c97ed2f83f7af26ab79e49724
Abel Nagy <nagy9219abel@gmail.com>
2018-02-06 14:05:46 +0100

    amenity fields

--- src/main/java/com/codecoool/rental/model/Amenity.java
+++ src/main/java/com/codecoool/rental/model/Amenity.java
@@ -1,4 +1,25 @@
 package com.codecoool.rental.model;
 
+import com.sun.istack.internal.NotNull;
+
+import javax.persistence.*;
+
+@Entity
 public class Amenity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne(mappedBy = "amenity")
+    private Rental rental;
+
+
+    @NotNull
+    private boolean hasWifi;
+
+    @NotNull
+    private boolean hasAirCondition;
+
+
 }
