ab7de89645c4f318a6015a999ed4c169e461a217
Abel Nagy <nagy9219abel@gmail.com>
2018-02-22 14:17:02 +0100

    first 3 test classes

--- src/main/test/com/codecoool/rental/services/RentalServiceTest.java
+++ src/main/test/com/codecoool/rental/services/RentalServiceTest.java
@@ -1,25 +1,15 @@
 package com.codecoool.rental.services;
 
 import com.codecoool.rental.RecordNotFoundException;
-import com.codecoool.rental.model.Rental;
 import com.codecoool.rental.model.Review;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 
-import javax.naming.ldap.HasControls;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RentalServiceTest {
@@ -36,14 +26,9 @@ public class RentalServiceTest {
         TypedQuery<Review> typedQuery = rentalServiceTest.em.createNamedQuery("getReviewById", Review.class).setParameter("id", 1);
         Review testReview = typedQuery.getSingleResult();
         assertEquals("awesome",testReview.getText());
-    }
 
-    @Test
-    public void getUpdateRentalReview() {
-    }
-
-    @Test
-    public void postUpdateRentalReview() {
+        em.close();
+        emf.close();
     }
 
     @Test
@@ -53,6 +38,9 @@ public class RentalServiceTest {
         this.rentalServiceTest = new RentalService(em);
 
         assertEquals(1, rentalServiceTest.getRental(1).get("id"));
+
+        em.close();
+        emf.close();
     }
 
     @Test
@@ -62,13 +50,8 @@ public class RentalServiceTest {
         this.rentalServiceTest = new RentalService(em);
 
         assertThrows(RecordNotFoundException.class, () -> rentalServiceTest.getRental(1000));
-    }
 
-    @Test
-    public void registerRental() {
-    }
-
-    @Test
-    public void getAllRentals() {
+        em.close();
+        emf.close();
     }
 }
---
+++ src/main/test/com/codecoool/rental/services/ReservationServiceTest.java
@@ -0,0 +1,65 @@
+package com.codecoool.rental.services;
+
+import com.codecoool.rental.RecordNotFoundException;
+import com.codecoool.rental.model.Rental;
+import com.codecoool.rental.model.Reservation;
+import com.codecoool.rental.model.ReservationPeriod;
+import com.codecoool.rental.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReservationServiceTest {
+
+    private ReservationService reservationServiceTest;
+
+    @InjectMocks
+    private Reservation reservation;
+
+    @Mock
+    private Rental rentalMock;
+
+    @Mock
+    private User userMock;
+
+    @Mock
+    private ReservationPeriod reservationPeriodMock;
+
+
+    @Test
+    public void submitReservationTest() throws RecordNotFoundException {
+        // Yes Miki, we tried
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("karalabeTest");
+        EntityManager em = emf.createEntityManager();
+        this.reservationServiceTest = new ReservationService(em);
+
+        assertFalse(reservationServiceTest.submitReservation("2017-07-21", "2017-07-28", "3", 1));
+
+        em.close();
+        emf.close();
+    }
+
+    @Test
+    public void getReservationsByUserId() throws RecordNotFoundException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("karalabeTest");
+        EntityManager em = emf.createEntityManager();
+        this.reservationServiceTest = new ReservationService(em);
+
+        when(userMock.getId()).thenReturn(1);
+        assertEquals(1, reservationServiceTest.getReservationsByUserId(userMock.getId()).size());
+
+        em.close();
+        emf.close();
+    }
+}
