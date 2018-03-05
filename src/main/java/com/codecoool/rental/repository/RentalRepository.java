e9cff1282358fdbef85d0d02e162df896be87ad7
Unknown <adamvermes@outlook.com>
2018-03-05 11:32:54 +0100

    repository base implemented

---
+++ src/main/java/com/codecoool/rental/repository/RentalRepository.java
@@ -0,0 +1,12 @@
+package com.codecoool.rental.repository;
+
+import com.codecoool.rental.model.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RentalRepository extends JpaRepository<Rental, Long>{
+
+}
+
+
